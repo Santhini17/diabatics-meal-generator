489572dc4360ff772e1973ffb60e4a81593ec694
Santhini17 <107545340+Santhini17@users.noreply.github.com>
2023-11-04 11:44:36 +0530

    Update server.js

--- node_server/server.js
+++ node_server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const openai = require('openai');
+const ConstraintSolver = require('python-constraint'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,18 +13,47 @@ const openaiAPIKey = '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';
 
 const openaiInstance = new openai({ key: openaiAPIKey });
 
-app.post('/predict', async (req, res) => {
+app.post('/suggest-meal', async (req, res) => {
     const { name, gender, dob, bmi, height, weight, bloodGlucoseLevel } = req.body;
 
-    const prompt = `Predict diabetic levels and suggest a meal plan for ${name}, a ${gender}, who has provided the following information:\n
-Name: ${name}
-Gender: ${gender}
-Date of Birth: ${dob}
-BMI: ${bmi}
-Height: ${height}
-Weight: ${weight}
-Blood Glucose Level: ${bloodGlucoseLevel}
-`;
+
+    const problem = new ConstraintSolver.Problem();
+
+    problem.addVariable('age', [30, 40, 50]);
+    problem.addVariable('bmi', [20, 25, 30]);
+    problem.addVariable('glucoseLevel', [80, 120, 160]);
+
+   
+    problem.addConstraint((age, dob) => age <= dob, ['age', 'dob']);
+    problem.addConstraint((bmi, userBmi) => bmi <= userBmi, ['bmi', 'userBmi']);
+    problem.addConstraint((glucoseLevel, bloodGlucoseLevel) => glucoseLevel <= bloodGlucoseLevel, ['glucoseLevel', 'bloodGlucoseLevel']);
+
+    // Solve the CSP to find valid assignments
+    const solutions = problem.solveAll();
+
+    // Use the solutions to generate meal plans
+
+    if (solutions.length === 0) {
+        res.status(400).json({ error: 'No valid solutions found' });
+        return;
+    }
+
+    const randomSolution = solutions[Math.floor(Math.random() * solutions.length)];
+
+    const { age, bmi, glucoseLevel } = randomSolution;
+
+    const prompt = `Suggest a meal plan for ${name}, a ${gender}, who has provided the following information:\n
+    Name: ${name}
+    Gender: ${gender}
+    Date of Birth: ${dob}
+    BMI: ${bmi}
+    Height: ${height}
+    Weight: ${weight}
+    Blood Glucose Level: ${bloodGlucoseLevel}
+    Age: ${age}
+    BMI Constraint: ${bmi}
+    Glucose Level Constraint: ${glucoseLevel}
+    `;
 
     try {
         const response = await openaiInstance.completions.create({
